b424371a7d31c4d2d8019203247463489424ea4d
marudor <marudor@marudor.de>
2016-01-17 10:53:08 +0100

    vex

---
+++ vex.js
@@ -0,0 +1,36 @@
+declare module 'vex' {
+  declare class VexDefaultOptions {
+    appendLocation?: ?string;
+    className?: ?string;
+    closeClassName?: ?string;
+    closeCSS?: Object;
+    content?: ?string;
+    contentClassName?: ?string;
+    contentCSS?: Object;
+    css?: Object;
+    escapeButtonCloses?: bool;
+    overlayClassName?: ?string;
+    overlayClosesOnClick?: bool;
+    overlayCSS?: Object;
+    showCloseButton?: bool;
+  }
+  declare class VexOptions extends VexDefaultOptions {
+    afterOpen?: (contentNode: Node) => any;
+    afterClose?: () => any;
+  }
+  declare class VexDialog {
+    static defaultOptions: VexDefaultOptions;
+    static alert(options: string|VexOptions): any;
+    static confirm(options: VexOptions): any;
+    static prompt(options: VexOptions): any;
+    static open(options: VexOptions): any;
+    static close(id: number): void;
+    static closeAll(): void;
+    static getAllVexes(): Array<string>;
+    static getVexByID(id: number): any;
+  }
+  declare class Vex extends VexDialog {
+    static dialog: typeof VexDialog;
+  }
+  declare var exports: typeof Vex;
+}
