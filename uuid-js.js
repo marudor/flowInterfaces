8179f5dfa881fd14611103abb9326c6e466ee8e5
marudor <marudor@marudor.de>
2016-01-17 10:53:32 +0100

    uuid-js

---
+++ uuid-js.js
@@ -0,0 +1,9 @@
+declare module 'uuid-js' {
+  declare class UUID {
+    toString(): string;
+    // fromParts(): string;
+    toURN(): string;
+    toBytes(): Array<any>;
+  }
+  declare function create(version?: 4|1): UUID;
+}
