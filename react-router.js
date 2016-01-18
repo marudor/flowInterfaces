f2c46e6cadc38aaaad5e950dd04c7220922299f4
marudor <marudor@marudor.de>
2016-01-18 14:16:38 +0100

    react-router

---
+++ react-router.js
@@ -0,0 +1,9 @@
+declare module 'react-router' {
+  declare class ReactRouter extends ReactClass {
+    Route: ReactClass;
+    IndexRoute: ReactClass;
+    Redirect: ReactClass;
+    Link: ReactClass;
+  }
+  declare var exports: ReactRouter;
+}
