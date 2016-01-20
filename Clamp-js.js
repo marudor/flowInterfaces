14461a89dd0b92b55acfbb45ce125ab5681aac45
tcn <t.clasen@schuchert.de>
2016-01-20 10:56:51 +0100

    [Clamp-js] correct optionsClamp

--- Clamp-js.js
+++ Clamp-js.js
@@ -1,11 +1,12 @@
+type ClampOptions = {
+  clamp?: number|string;
+  useNativeClamp?: bool,
+  truncationChar?: string,
+  truncationHTML?: string,
+  splitOnChars?: Array<string>,
+  animate?: bool,
+}
+
 declare module 'Clamp-js' {
-  declare interface ClampOptions {
-    clamp?: (number|string);
-    useNativeClamp?: bool,
-    truncationChar?: string,
-    truncationHTML?: string,
-    splitOnChars?: Array<string>,
-    animate?: bool,
-  }
   declare function exports(element: HTMLElement, options?: ClampOptions): void;
 }
