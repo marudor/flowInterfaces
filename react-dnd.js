8351ec5e493fb956a770efd7bb0f0b6b4bb909c6
marudor <marudor@marudor.de>
2016-01-27 16:08:28 +0100

    <react-dnd> correct Decorator signature

--- react-dnd.js
+++ react-dnd.js
@@ -1,12 +1,14 @@
 declare module 'react-dnd' {
-  declare function DropTarget(compoennt: ReactClass): ReactClass;
-  declare function DragSource(compoennt: ReactClass): ReactClass;
-  declare function DragLayer(compoennt: ReactClass): ReactClass;
+  declare function DropTarget(type: string|string[], spec?: Object, collect?: Object, options?: any): (component: ReactClass) => ReactClass;
+  declare function DragSource(type: string|string[], spec?: Object, collect?: Object, options?: any): (component: ReactClass) => ReactClass;
+  declare function DragLayer(type: string|string[], spec?: Object, collect?: Object, options?: any): (component: ReactClass) => ReactClass;
   declare function DragDropContext(backend: any): ReactClass;
 }
 
 
 declare module 'react-dnd-html5-backend' {
-  declare function exports(): any;
-  declare function getEmptyImage(): any;
+  declare class HTML5Backend {
+    getEmptyImage(): any;
+  }
+  declare var exports: HTML5Backend;
 }
