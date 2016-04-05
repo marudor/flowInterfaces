857bb6156fd60c9e5dbf6e466967e07bbec32801
Steven Bassett <steven.j.bassett@gmail.com>
2016-04-05 09:31:33 -0700

    Test: create some tests for  material-ui

---
+++ packages/iflow-material-ui/fixtures/fail.js
@@ -0,0 +1,6 @@
+// @flow
+import { AppBar } from 'material-ui';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+
+const element = <AppBar className={false} showMenuIconButton={null} />;
---
+++ packages/iflow-material-ui/test.js
@@ -0,0 +1,29 @@
+import { execFile } from 'child_process';
+import flow from 'flow-bin';
+import { expect } from 'chai';
+import path from 'path';
+
+describe('Material UI', function() {
+  this.timeout(6 * 1000);
+  it('should be able to tell the props of the app bar', (done) => {
+      execFile(flow, ['check', path.resolve(__dirname, './fixtures/fail.js')], (err, stdout) => {
+        expect(err).to.be.defined;
+        expect(stdout).to.contain(
+` 6: const element = <AppBar className={false} showMenuIconButton={null} />;
+                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ React element \`AppBar\`
+  6: const element = <AppBar className={false} showMenuIconButton={null} />;
+                                        ^^^^^ boolean. This type is incompatible with
+ 14:         className?: string,
+                         ^^^^^^ string. See: index.js.flow:14
+
+fixtures/fail.js:6
+  6: const element = <AppBar className={false} showMenuIconButton={null} />;
+                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ props of React element \`AppBar\`
+  6: const element = <AppBar className={false} showMenuIconButton={null} />;
+                                                                   ^^^^ null. This type is incompatible with
+ 92:         showMenuIconButton: true,
+                                 ^^^^ boolean literal \`true\`. See: index.js.flow:92`);
+        done();
+      });
+  });
+});
