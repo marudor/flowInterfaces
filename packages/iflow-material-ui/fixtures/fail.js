6408c08c9218f0ca53c89372dee730b160dbdb5b
marudor <marudor@marudor.de>
2016-07-30 15:25:57 +0200

    fix orderBy

--- packages/iflow-material-ui/fixtures/fail.js
+++
@@ -1,6 +0,0 @@
-// @flow
-import { AppBar } from 'material-ui';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-
-const element = <AppBar className={false} showMenuIconButton={null} />;
