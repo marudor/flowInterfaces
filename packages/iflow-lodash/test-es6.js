4610b83275d136ab7c5df3bfa04f3faf9c45e04b
Ryan Graham <r.m.graham@gmail.com>
2016-07-31 16:05:05 -0700

    lodash: add _.concat() definition

--- packages/iflow-lodash/test-es6.js
+++ packages/iflow-lodash/test-es6.js
@@ -4,7 +4,7 @@
 import lodash from 'lodash';
 
 // sanity check for named exports
-import {map} from 'lodash';
+import {concat, map} from 'lodash';
 
 var nums : number[] = [1,2,3,4,5,6];
 
@@ -15,6 +15,15 @@ var directSquares : number[];
 
 var nativeStrings : string[];
 var directStrings : string[];
+
+var allNums : number[];
+var numsAndStrList : Array<number|string>;
+var mixedList : Array<mixed>;
+allNums = concat(nums, nums, nums);
+numsAndStrList = concat(nums, '123', '456');
+numsAndStrList = concat(nums, ['123', '456']);
+numsAndStrList = concat(nums, [[1,2,3], '456']);
+mixedList = concat(nums, [[1,2,3], '456']);
 
 // Array#map, lodash.map, lodash#map
 nativeSquares = nums.map(function(num) {
--- packages/iflow-lodash/test.js
+++ packages/iflow-lodash/test.js
@@ -2,6 +2,7 @@
 
 var lodash = require('lodash');
 var map = require('lodash').map;
+var concat = require('lodash').concat;
 
 var nums : number[] = [1,2,3,4,5,6];
 
@@ -12,6 +13,15 @@ var directSquares : number[];
 
 var nativeStrings : string[];
 var directStrings : string[];
+
+var allNums : number[];
+var numsAndStrList : Array<number|string>;
+var mixedList : Array<mixed>;
+allNums = concat(nums, nums, nums);
+numsAndStrList = concat(nums, '123', '456');
+numsAndStrList = concat(nums, ['123', '456']);
+numsAndStrList = concat(nums, [[1,2,3], '456']);
+mixedList = concat(nums, [[1,2,3], '456']);
 
 // Array#map, lodash.map, lodash#map
 nativeSquares = nums.map(function(num) {
